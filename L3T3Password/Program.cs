dc4ada9078e7fcfbd4f4bf39c7950486c0746ff7
Mary250305 <slainter@mail.ru>
2022-12-14 17:34:26 +0700

    Написала программу для проверки пароля

---
+++ L3T3Password/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace L3T3
+{
+    class L3T3
+    {
+        static void Main()
+        {
+            string password = "Masha";
+            int pw = password.Length;
+            string parol = "Mariya";
+            if (pw> parol.Length)
+            {
+                Console.WriteLine("Пароль неверный строка слишком короткая");
+            }
+            else if (pw == parol.Length)
+            {
+                Console.WriteLine("Пароль верный");
+            }
+            else
+            {
+                Console.WriteLine("Пароль неверный строка слишком длинная");
+            }
+        }
+    }
+}
