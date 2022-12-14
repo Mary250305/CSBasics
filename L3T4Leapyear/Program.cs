c5694a1d4a11fbe068b938d1c5862e7c86f716ae
Mary250305 <slainter@mail.ru>
2022-12-14 17:56:30 +0700

    Программа для проверки года

---
+++ L3T4Leapyear/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace L3Y4
+{
+    class L3T4
+    {
+        static void Main()
+        {
+            int year = 1675;
+            if ((year % 400 == 0) || ((year % 4 == 0) && (year % 100 != 1)))
+            {
+                Console.WriteLine("Високосный год");
+            }
+            else
+            {
+                Console.WriteLine("Не високосный год");
+            }
+        }
+    }
+}
