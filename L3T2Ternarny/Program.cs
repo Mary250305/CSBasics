d53f865d4c8e4429537917fcff653f94a00bab2e
Mary250305 <slainter@mail.ru>
2022-12-14 17:11:10 +0700

    Сделала программу с помощью тернального оператора

---
+++ L3T2Ternarny/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace L3T2
+{
+    class L3T2
+    {
+        static void Main()
+        {
+            int a = 23;
+            int b = 34;
+            int max = (a > b) ? a : b;
+            int min = (a < b) ? a : b;
+            Console.WriteLine(max);
+            Console.WriteLine(min);
+        }
+    }
+}
