c3f7113bc611ae7b6985df28ae986e9f67a8c8a8
Mary250305 <slainter@mail.ru>
2022-12-14 16:38:45 +0700

    дз

--- L3T1Chains/Program.cs
+++ L3T1Chains/Program.cs
@@ -1,2 +1,24 @@
-﻿using System;
-using System.Reflection.Metadata;
+﻿
+namespace L3T1
+{
+    class L3T1
+    {
+        [STAThread]
+        static void Main()
+        {
+            int a = 6;
+            if (a > 0)
+            {
+                Console.WriteLine("Положительное число");
+            }
+            else if (a == 0)
+            {
+                Console.WriteLine("Число = 0");
+            }
+            else
+            {
+                Console.WriteLine("Отрицательное число");
+            }
+        }
+    }
+}
