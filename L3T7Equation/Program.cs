4af884623f27b678a6fd667d1e7bb5e4df1b16ad
Mary250305 <slainter@mail.ru>
2022-12-15 22:47:31 +0700

    Программа для решения квадратного уравнения

---
+++ L3T7Equation/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace L3T6
+{
+    class L3T6
+    {
+        private static double x1;
+        private static double x2;
+
+        static void Main()
+        {
+            Console.WriteLine("Введите коэфициент а");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите коэфициент b");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите с");
+            int c = Convert.ToInt32(Console.ReadLine());
+            int D = (int) Math.Pow(b, 2) - 4 * a * c;
+
+            if (a > 0 || b > 0 || c > 0)
+            {
+                if (D>0)
+                {
+                    x1 = (-b + Math.Sqrt(D)) / (2 * a);
+                    x2 = (-b - Math.Sqrt(D)) / (2 * a);
+                    Console.WriteLine(Math.Round(x1));
+                    Console.WriteLine(Math.Round(x2));
+                }
+                else if (D==0)
+                {
+                    x1 = (-b) / (2 * a);
+                    Console.WriteLine(Math.Round(x1));
+                }
+                else
+                {
+                    Console.WriteLine("Корней нет");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Уравнение не имеет значеня");
+            }
+            
+        }
+    }
+}
