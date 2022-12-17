494c9f14ae6541504edd1a671261fd0920135bdc
Mary250305 <slainter@mail.ru>
2022-12-17 23:24:45 +0700

    Программа на определение возраста

--- L3T6Age/Program.cs
+++ L3T6Age/Program.cs
@@ -1,30 +1,38 @@
-﻿namespace L3T6
+﻿namespace L3T6Age
 {
-    class L3T6
+    class Program
     {
         static void Main()
         {
-            Console.WriteLine("Введите свой возраст ");
+            Console.Write("Введите свой возраст: ");
             int age = Convert.ToInt32(Console.ReadLine());
 
             if (age < 1)
             {
                 Console.WriteLine("Вы слишком малы");
             }
+
             else if (age > 112)
             {
                 Console.WriteLine("Вы слишком стары");
             }
 
-            while (1<age<112);
+
             if (age % 10 == 1)
             {
                 Console.WriteLine("Вам " + age + " год");
             }
+
+            else if (age > 10 && age < 20)
+            {
+                Console.WriteLine("Вам " + age + " лет");
+            }
+
             else if ((age % 10 == 2) || (age % 10 == 3) || (age % 10 == 4))
             {
                 Console.WriteLine("Вам " + age + " года");
             }
+
             else if ((age % 10 == 5) || (age % 10 == 6) || (age % 10 == 7) || (age % 10 == 8) || (age % 10 == 9) || (age % 10 == 0))
             {
                 Console.WriteLine("Вам " + age + " лет");
@@ -33,4 +41,3 @@
     }
 }
 
- 
