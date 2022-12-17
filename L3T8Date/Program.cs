9c146b3dd747891c0d749f28a8efd33425973fb0
Mary250305 <slainter@mail.ru>
2022-12-18 00:06:08 +0700

    Программа для написания дат

--- L3T8Date/Program.cs
+++ L3T8Date/Program.cs
@@ -1,26 +1,64 @@
-﻿namespace L3T8
+﻿namespace L3T8Date
 {
-    class L3T8
+    class Program
     {
         static void Main()
         {
-            Console.WriteLine("Введие день");
+            Console.Write("Введие день: ");
             int day = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Введите месяц");
+            Console.Write("Введите месяц: ");
             int month = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Введение год");
+            Console.Write("Введение год: ");
             int year = Convert.ToInt32(Console.ReadLine());
 
-            if (day < 32 && month < 13)
+            if (day < 32 && month < 12)
             {
-                if (day> 31 && month <12)
+                if (day == 30 && (month == 4 || month == 6 || month == 9 || month == 11))
                 {
                     month = month + 1;
-                    year = year + 1;
+                    day = 1;
                     Console.WriteLine(day + "." + month + "." + year);
                 }
+
+
+                else if (day ==28 && month ==2 )
+                {
+                    month = month + 1;
+                    day = 1;
+                    Console.WriteLine(day + "." + month + "." + year);
+                }
+
+
+                else if (day == 31 && (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10))
+                {
+                    month= month + 1;
+                    day = 1;
+                    Console.WriteLine(day + "." + month + "." + year);
+                }
+
+
+                else if (day < 30 && (month == 4 || month == 6 || month == 9 || month == 11))
+                {
+                    day = day + 1;
+                    Console.WriteLine(day + "." + month + "." + year);
+                }
+
+
+                else if ( day < 31 && (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10))
+                {
+                    day = day+ 1;
+                    Console.WriteLine(day + "." + month + "." + year);
+                }
+            }
+
+            else if (day == 31 &&  month == 12)
+            {
+                month = 1;
+                day = 1;
+                year = year + 1;
+                Console.WriteLine(day + "." + month + "." + year);
             }
         }
     }
