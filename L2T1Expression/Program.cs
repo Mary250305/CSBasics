02e615e1c0150fe24c72442a94d51859ea01b85d
Mary250305 <slainter@mail.ru>
2022-12-19 18:42:38 +0700

    Решение примера

--- L2T1Expression/Program.cs
+++ L2T1Expression/Program.cs
@@ -6,8 +6,8 @@
         {
             int x = 3 - ((56 - 12) / 44) * (4 / 2);
             int y = (2 * x) / (33 - x);
-           
-            Console.WriteLine(y);
+            int z = (-x) / (2 * y);
+            Console.WriteLine(z);
         }
     }
 }
