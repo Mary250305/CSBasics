36ee01654647195684c076765bccaa2f70ce4326
Mary250305 <slainter@mail.ru>
2022-12-19 20:47:01 +0700

    Программа на проверку равенства вещественных чисел

---
+++ L4T1Eqality/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Test
+{
+    class Test
+    {
+        static void Main()
+        {
+            Console.Write("Введите число: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введите число: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+            double epsilon = 1.0e-10;
+
+            if (Math.Abs(a - b) <= epsilon)
+            {
+                Console.WriteLine("Они равны");
+            }
+            else
+            {
+                Console.WriteLine("Они не равны");
+            }
+        }
+    }
+}
