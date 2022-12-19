b0997d7ded4e450960ffbc2e9295e45aec2ca451
Mary250305 <slainter@mail.ru>
2022-12-20 04:43:00 +0700

    Программа на нахождение ср. знач через цикл ду ваил

---
+++ L4T6ArithmeticMean/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace L4T5ArithmeticMean
+{
+    class Program
+    {
+        static void Main()
+        {
+            int i = 3;
+            int sum = 3;
+            int quantity = 0;
+
+            do
+            {
+                sum += i;
+                quantity += 1;
+                i += 1;
+            }
+            while (i < 54);
+
+            double arithmeticMean = sum / quantity;
+            Console.WriteLine(Math.Round(arithmeticMean));
+        }
+    }
+}
