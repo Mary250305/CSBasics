69c2c03e08424746bb17e89f5e1d2b40c8e0a6eb
Mary250305 <slainter@mail.ru>
2022-12-19 20:37:31 +0700

    Программа для решения квадратного уравнения

--- L3T7Equation/Program.cs
+++ L3T7Equation/Program.cs
@@ -8,38 +8,35 @@
             Console.WriteLine();
 
             Console.Write("Введите а: ");
-            int a = Convert.ToInt32(Console.ReadLine());
+            int numberA = Convert.ToInt32(Console.ReadLine());
 
             Console.Write("Введите b: ");
-            int b = Convert.ToInt32(Console.ReadLine());
+            int numberB = Convert.ToInt32(Console.ReadLine());
 
             Console.Write("Введите с: ");
-            int c = Convert.ToInt32(Console.ReadLine());
+            int numberC = Convert.ToInt32(Console.ReadLine());
 
-            int discriminant = (int) Math.Pow(b, 2) - 4 * a * c;
+            int discriminant = (int) Math.Pow(numberB, 2) - 4 * numberA * numberC;
 
-            if (a > 0 || b > 0 || c > 0)
+            if (numberA > 0 || numberB > 0 || numberC > 0)
             {
                 if (discriminant>0)
                 {
-                    double root1 = (-b + Math.Sqrt(discriminant)) / (2 * a);
-                    double root2 = (-b - Math.Sqrt(discriminant)) / (2 * a);
+                    double root1 = (-numberB + Math.Sqrt(discriminant)) / (2 * numberA);
+                    double root2 = (-numberB - Math.Sqrt(discriminant)) / (2 * numberA);
                     Console.WriteLine(Math.Round(root1));
                     Console.WriteLine(Math.Round(root2));
                 }
-
                 else if (discriminant==0)
                 {
-                    double root = (-b) / (2 * a);
+                    double root = (-numberB) / (2 * numberA);
                     Console.WriteLine(root);
                 }
-
                 else
                 {
                     Console.WriteLine("Корней нет");
                 }
             }
-
             else
             {
                 Console.WriteLine("Уравнение не имеет значеня");
