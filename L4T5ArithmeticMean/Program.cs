693ac45546c4caac510135442d9c296e3509c9c5
Mary250305 <slainter@mail.ru>
2022-12-20 04:36:25 +0700

    Программа на нахождение ср. знач

---
+++ L4T5ArithmeticMean/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace L4T5ArithmeticMean
+{
+    class Program
+    {
+        static void Main()
+        {
+            int i = 3;
+            int sum = 3;
+            int quantity = 0;
+
+            while (i < 54)
+            {
+                sum += i;
+                quantity += 1;
+                i += 1;
+            }
+
+            double arithmeticMean = sum / quantity;
+            Console.WriteLine(Math.Round(arithmeticMean));
+        }
+    }
+}
