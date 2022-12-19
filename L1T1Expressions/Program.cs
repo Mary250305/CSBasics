4a91db605c7ec67237ee81dc4f518826ebb7a4e4
Mary250305 <slainter@mail.ru>
2022-12-19 18:27:45 +0700

    Программа для решения примера

---
+++ L1T1Expressions/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace L1T1Expressions
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Введите переменную 1: ");
+            int variable1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите переменную 2: ");
+            int variable2 = Convert.ToInt32(Console.ReadLine());
+
+            int expressions = (variable1 + variable2) * (variable1 - variable2);
+            Console.WriteLine(expressions);
+        }
+    }
+}
