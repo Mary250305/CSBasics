2892278cfd2934d0e5ee9af09fdb14ef05636f87
Mary250305 <slainter@mail.ru>
2022-12-19 22:28:17 +0700

    Программа на отработку цикла ваил

---
+++ L4T4While/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace L4T4While
+{
+    class Program
+    {
+        static void Main()
+        {
+            /*int i = 0;
+            int sum = 0;
+            while (i < 10)
+            {
+                sum += i;
+                i += 1;
+            }
+            Console.WriteLine(sum);*/
+
+            /*int i = 3;
+            int sum = 3;
+            while (i < 22)
+            {
+                sum += i;
+                i += 1;
+            }
+            Console.WriteLine(sum);*/
+
+            /*int i = 3;
+            int sum = 3;
+            while (i < 22)
+            {
+                if (i % 2 == 0)
+                {
+                    sum += i;
+                }
+                i += 1;
+            }
+            Console.WriteLine(sum);*/
+
+            int i = 3;
+            int sum = 3;
+            int evenNumbers = 0;
+            while (i < 22)
+            {
+                if (i % 2 == 0)
+                {
+                    sum += i;
+                    evenNumbers += 1;
+                }
+                i += 1;
+            }
+            Console.WriteLine(sum);
+            Console.WriteLine("Кол-во четных чисел = " + evenNumbers);
+        }
+    }
+}
