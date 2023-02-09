6a6b349535d4b3dd93292588396ed2878eccc08e
Mary250305 <slainter@mail.ru>
2023-02-09 22:52:39 +0700

    Cср знач через ду ваил

---
+++ L4T8DoWhile/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace L4T8DoWhile
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Введите первое число диапазона: ");
+            int initialNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите конечное число диапазона: ");
+            int finiteNumber = Convert.ToInt32(Console.ReadLine());
+
+            int quantity = 0;
+            int sum = 0;
+            int i = initialNumber;
+
+            do
+            {
+                sum += i;
+                quantity += 1;
+                i += 1;
+            }
+            while (i <= finiteNumber);
+
+            double arithmeticMean = (double)sum / quantity;
+            Console.WriteLine("Среднее арефмитическое = " + arithmeticMean);
+        }
+    }
+}
