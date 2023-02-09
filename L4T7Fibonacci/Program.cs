368371ff7836a2d413a4b272a635fed83375fe31
Mary250305 <slainter@mail.ru>
2023-02-09 22:36:47 +0700

    Задача на числа Фибаначчи

--- L4T7Fibonacci/Program.cs
+++ L4T7Fibonacci/Program.cs
@@ -4,7 +4,33 @@
     {
         static void Main()
         {
+            Console.Write("Введите целое число: ");
+            int number = Convert.ToInt32(Console.ReadLine());
 
+            int f0 = 0;
+            int f1 = 1;
+            int fibonacci = 0;
+            int i = 2;
+
+            if (number == 0)
+            {
+                fibonacci = 0;
+            }
+            else if (number == 1)
+            {
+                fibonacci = 1;
+            }
+            else
+            {
+                while (i <= number)
+                {
+                    fibonacci = f0 + f1;
+                    f0 = f1;
+                    f1 = fibonacci;
+                    i += 1;
+                }
+            }
+            Console.WriteLine("Число Фибаначчи под номером {1} = {0} ", fibonacci, number);
         }
     }
 }
