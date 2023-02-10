d72056dfd67bcdfcdeb716833ed461615755df0f
Mary250305 <slainter@mail.ru>
2023-02-10 17:51:29 +0700

    Задача на 10 чисел в столбик

---
+++ L4T9NumbersPerLine/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace L4T9NumberPerLine
+{
+    class Program
+    {
+        static void Main()
+        {
+            int i = 1;
+            while (i <= 100)
+            {
+                Console.Write(i.ToString().PadLeft(10));
+                if (i % 10 == 0)
+                {
+                    Console.WriteLine();
+                }
+                i += 1;
+            }
+        }
+    }
+}
