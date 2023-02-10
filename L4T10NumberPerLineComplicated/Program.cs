d87d3e1f9eca4f2e45871e782615abb5f0f4c568
Mary250305 <slainter@mail.ru>
2023-02-10 18:02:32 +0700

    10 чисел в строке сложная

---
+++ L4T10NumberPerLineComplicated/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace L4T9NumberPerLineComplicated
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Введите начальное число: ");
+            int initialNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите конечное число диапазона: ");
+            int finiteNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите количество строк: ");
+            int quantity = Convert.ToInt32(Console.ReadLine());
+
+            int i = initialNumber;
+
+            while (i <= finiteNumber)
+            {
+                Console.Write(i.ToString().PadLeft(quantity));
+                if (i % quantity == 0)
+                {
+                    Console.WriteLine();
+                }
+                i += 1;
+            }
+        }
+    }
+}
