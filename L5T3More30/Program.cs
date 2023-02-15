4dac95d9344c884f19508877fc302d3b71c37fb4
Mary250305 <slainter@mail.ru>
2023-02-15 19:24:37 +0700

    числа до 30

---
+++ L5T3More30/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace L5T3More30
+{
+    class Pogram
+    {
+        static void Main()
+        {
+            for (int i = 1; i <=100; i++) 
+            {
+                if ( i > 29)
+                {
+                    break;
+                }
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
