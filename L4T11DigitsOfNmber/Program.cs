99d6a9a6d0f980777cfc8d015fcb67b7e908e79b
Mary250305 <slainter@mail.ru>
2023-02-15 18:47:06 +0700

    задача распечатать числа от до

---
+++ L4T11DigitsOfNmber/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace L4T11DigitsOfNumber
+{
+    class Program
+    {
+        static void Main()
+        {
+
+        }
+    }
+}
---
+++ L5T1numbersFromAndTo/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace L5T1numbersFromAndTo
+{
+    class Program
+    {
+        static void Main()
+        {
+            for (int i = 0; i <= 100; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine();
+
+            for (int j = 7; j <= 121; j++)
+            {
+                Console.WriteLine(j);
+            }
+
+            Console.WriteLine();
+
+            for (int k = 100; k >= 30; k--)
+            {
+                Console.WriteLine(k);
+            }
+        }
+    }
+}
