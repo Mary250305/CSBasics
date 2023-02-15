31d8d1aa2fa1463bed6cb0fa3ca2d56d0eb80ae7
Mary250305 <slainter@mail.ru>
2023-02-15 19:16:00 +0700

    ср знач через фор

---
+++ L5T2ArithmeticMean/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace L5T2ArithmeticMean
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Введите первое число диапазона: ");
+            int initialNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите конечное число диапазона: ");
+            int finiteNumber = Convert.ToInt32(Console.ReadLine());
+
+            int quantity = 0;
+            int sum = 0;
+
+            for (int i = initialNumber; i <= finiteNumber; i++) 
+            {
+                sum += i;
+                quantity += 1;
+            }
+            double arithmeticMean = (double)sum / quantity;
+            Console.WriteLine("Среднее арефмитическое = " + arithmeticMean);
+
+            int sumEvnNumber = 0;
+            int quantityEvenNumber = 0;
+
+            for (int j = initialNumber; j <= finiteNumber; j++)
+            {
+                if (j % 2 == 0)
+                {
+                    sumEvnNumber += j;
+                    quantityEvenNumber += 1;
+                }
+            }
+
+            double arithmeticMeanEvenNumber = (double)sumEvnNumber / quantityEvenNumber;
+            Console.WriteLine("Среднее арефмитическое четных чисел = " + arithmeticMeanEvenNumber);
+        }
+    }
+}
