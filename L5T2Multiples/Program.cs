e0f7f7d1a43f079bdb2bef8c5675be677a34324d
Mary250305 <slainter@mail.ru>
2023-02-15 19:03:36 +0700

    квадраты чисел

---
+++ L5T2Multiples/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+
+namespace L5T2Multiples
+{
+    class Program
+    {
+        static void Main()
+        {
+            for ( int i = 100; i >= 1; i-- )
+            {
+                Console.WriteLine( i );
+            }
+
+            Console.WriteLine();
+
+            int number = 5;
+            for (int j = 1; j <= number; j++)
+            {
+                Console.WriteLine(Math.Pow( j, 2 ) );
+            }
+        }
+    }
+}
