6f47dab64504aca7dd1bb12746e96ecf2f7b5bf0
Mary250305 <slainter@mail.ru>
2023-02-18 17:53:36 +0700

    функции

---
+++ L5T4Continue/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace L5T4Continue
+{
+    class Pogram
+    {
+        static void Main()
+        {
+           /*for (int i = 0; i <= 100; i++)
+            {
+                if (i == 5 || i % 3 == 0 || (i >= 60 && i <= 80))
+                {
+                    continue;
+                }
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine();*/
+
+            int j = 0;
+            while (j <= 100)
+            {
+                if (j == 5 || j % 3 == 0 || (j >= 60 && j <= 80))
+                {
+                    ++j;
+                    continue;
+                }
+                ++j;
+                Console.WriteLine(j);
+            }
+        }
+    }
+}
---
+++ L6T1/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Test
+{
+    class Test
+    {
+        public static void Main()
+        {
+            Console.WriteLine(GetSolution(3, 5));
+            Console.WriteLine(GetSolution(4, 5));
+            Console.WriteLine(GetArithmeticMean(1, 2));
+            Console.WriteLine(GetMaximum(1, 3));
+            Console.WriteLine(GetMinimum(1, 3));
+        }
+
+        public static double GetSolution(int x, int y)
+        {
+            return 3 * x + 4 * y;
+        }
+
+        public static double GetArithmeticMean(int begin, int end)
+        {
+            int sum = 0;
+            int quantity = 0;
+
+            for (int i = begin; i <= end; i++)
+            {
+                sum += i;
+                quantity += 1;
+            }
+            return (double)sum / quantity;
+        }
+
+        public static double GetMaximum(int x, int y)
+        {
+            return Math.Max(x, y);
+        }
+
+        private static double GetMinimum( int x, int y)
+        {
+            return Math.Min(x, y);
+        }
+    }
+}
