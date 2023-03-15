e911f7717a8e3e4a448c7263e9a619fd13fe49ef
Mary250305 <slainter@mail.ru>
2023-03-15 15:31:41 +0700

    простые числа

---
+++ L5T6PrimeNumbers/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace L5T6PrimeNumbers
+{
+    class Progrem
+    {
+
+        public static void Main()
+        {
+            Console.Write("Введите число: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i <= number; i++)
+            {
+                if (IsPrime(i))
+                {
+                    Console.Write(i.ToString() + "\n");
+                }
+            }
+        }
+        public static bool IsPrime(int N)
+        {
+            for (int i = 2; i <=(N / 2); i++)
+            {
+                if (N % i == 0)
+                    return false;
+            }
+            return true;
+        }
+    }
+}
