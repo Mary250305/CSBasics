e97a583075a3762d7d1dee18979394ec189baa3b
Mary250305 <slainter@mail.ru>
2023-03-15 14:45:24 +0700

    программа брейк

---
+++ L5T5Break/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace L5T5Break
+{
+    class Pogram
+    {
+        static void Main()
+        {
+            for (; ; )
+            {
+                Console.WriteLine();
+                Console.Write("Введите слово: ");
+                string line = Convert.ToString(Console.ReadLine());
+
+                string inventedLine = "рука";
+
+                if (line == inventedLine)
+                {
+                    Console.WriteLine("Верно");
+                    break;
+                }
+                Console.Write("Неверно, попробуйте снова");
+            }
+        }
+    }
+}
