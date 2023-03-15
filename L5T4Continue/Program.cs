08096df52e85e17dd3d921e26ede58dc04549582
Mary250305 <slainter@mail.ru>
2023-03-15 14:04:45 +0700

    .

--- L5T4Continue/Program.cs
+++ L5T4Continue/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main()
         {
-           /*for (int i = 0; i <= 100; i++)
+           for (int i = 0; i <= 100; i++)
             {
                 if (i == 5 || i % 3 == 0 || (i >= 60 && i <= 80))
                 {
@@ -13,12 +13,12 @@
                 Console.WriteLine(i);
             }
 
-            Console.WriteLine();*/
+            Console.WriteLine();
 
             int j = 0;
             while (j <= 100)
             {
-                if (j == 5 || j % 3 == 0 || (j >= 60 && j <= 80))
+                if (j == 5 )
                 {
                     ++j;
                     continue;
