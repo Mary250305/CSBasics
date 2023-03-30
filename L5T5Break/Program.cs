da4c37ad9eb50f2ca5de4f11937804c9a72db123
Mary250305 <slainter@mail.ru>
2023-03-30 13:04:18 +0700

    прога на перевод градусов

--- L5T5Break/Program.cs
+++ L5T5Break/Program.cs
@@ -8,7 +8,7 @@
             {
                 Console.WriteLine();
                 Console.Write("Введите слово: ");
-                string line = Convert.ToString(Console.ReadLine());
+                string line = Console.ReadLine();
 
                 string inventedLine = "рука";
 
---
+++ L6T3Degrees/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace L6T3Degrees
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine(ConversionKelvin(5));
+            Console.WriteLine(ConversionFahrenheit(5));
+        }
+
+        public static double ConversionKelvin(int x)
+        {
+            double T = 273.15;
+            return (double) (x + T);
+        }
+
+        public static double ConversionFahrenheit (int x)
+        {
+            return (double)(1.8 * x + 32);
+        }
+    }
+}
