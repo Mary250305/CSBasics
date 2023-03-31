eb1bc73011fd524f2f212fbf37689c2ec7acb073
Mary250305 <slainter@mail.ru>
2023-03-31 16:09:53 +0700

    таблица умножения

--- L5T8Table/Program.cs
+++ L5T8Table/Program.cs
@@ -6,21 +6,19 @@
         {
             Console.Write("До какого числа вывести таблицу: ");
             int number = Convert.ToInt32(Console.ReadLine());
-            int result = 0;
+          
             Console.WriteLine(string.Empty.PadLeft(number*10, '-'));
 
             for (int i = 1; i <= number; i++)
             {
                 for (int j = 1; j <= number; j++)
                 {
-                    result = i * j;
+                    int result = i * j;
                     Console.Write(result.ToString().PadRight(10));
                 }
                 Console.WriteLine();
+                Console.WriteLine(string.Empty.PadLeft(number * 10, '-'));
             }
-
-            Console.WriteLine();
-            Console.WriteLine(string.Empty.PadLeft(number*10, '-'));
         }
     }
 }
