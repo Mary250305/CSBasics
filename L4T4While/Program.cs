aae21269f3659e285314a2e8c2aa542861a60c1d
Mary250305 <slainter@mail.ru>
2023-04-11 17:25:43 +0700

    прога суммы 4 разных вида

--- L4T4While/Program.cs
+++ L4T4While/Program.cs
@@ -4,49 +4,55 @@
     {
         static void Main()
         {
-            /*int i = 0;
+            int i = 0;
             int sum = 0;
             while (i < 10)
             {
                 sum += i;
                 i += 1;
             }
-            Console.WriteLine(sum);*/
+            Console.WriteLine(sum);
 
-            /*int i = 3;
-            int sum = 3;
-            while (i < 22)
+            Console.WriteLine();
+
+            int j = 3;
+            int sum1 = 3;
+            while (j < 22)
             {
-                sum += i;
-                i += 1;
+                sum1 += j;
+                j += 1;
             }
-            Console.WriteLine(sum);*/
+            Console.WriteLine(sum1);
 
-            /*int i = 3;
-            int sum = 3;
-            while (i < 22)
+            Console.WriteLine();
+
+            int k = 3;
+            int sum2 = 3;
+            while (k < 22)
             {
-                if (i % 2 == 0)
+                if (k % 2 == 0)
                 {
-                    sum += i;
+                    sum2 += k;
                 }
-                i += 1;
+                k += 1;
             }
-            Console.WriteLine(sum);*/
+            Console.WriteLine(sum2);
 
-            int i = 3;
-            int sum = 3;
+            Console.WriteLine();
+
+            int m = 3;
+            int sum3 = 3;
             int evenNumbers = 0;
-            while (i < 22)
+            while (m < 22)
             {
-                if (i % 2 == 0)
+                if (m % 2 == 0)
                 {
-                    sum += i;
+                    sum3 += m;
                     evenNumbers += 1;
                 }
-                i += 1;
+                m += 1;
             }
-            Console.WriteLine(sum);
+            Console.WriteLine(sum3);
             Console.WriteLine("Кол-во четных чисел = " + evenNumbers);
         }
     }
