d0c4c399885e259cf6d7e46d40c3edd0a1078669
Mary250305 <slainter@mail.ru>
2023-04-11 17:36:53 +0700

    числа в строку сложная

---
+++ L4T9NumberPerLine2/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace L4T9NumberPerLine2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("От какого числа вывести таблицу: ");
+            int initialNumber = Convert.ToInt32(Console.ReadLine());
+            Console.Write("До какого числа вывести таблицу: ");
+            int finiteNumber = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Какое количество чисел в одной стрке: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            while (initialNumber <= finiteNumber)
+            {
+                Console.Write(initialNumber.ToString().PadLeft(10));
+                if (initialNumber % number == 0)
+                {
+                    Console.WriteLine();
+                }
+                initialNumber += 1;
+            }
+        }
+    }
+}
