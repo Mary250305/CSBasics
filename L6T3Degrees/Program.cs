2689d00ca4d4d46f3ef10cb9b0da83fddb86d50e
Mary250305 <slainter@mail.ru>
2023-05-02 16:07:43 +0700

    перевод температуры

--- L6T3Degrees/Program.cs
+++ L6T3Degrees/Program.cs
@@ -6,17 +6,17 @@
         {
             Console.Write("Введите температуру в градусах Цельсия для перевода в градусы Кельвина: ");
             Console.WriteLine("Температура по шкале Кельвина: " + ConversionKelvin(Convert.ToDouble(Console.ReadLine())));
+
             Console.Write("Введите температуру в градусах Цельсия для перевода в градусы Фаренгейта: ");
-            Console.WriteLine("Температура по шкале Фаренгейта: " +ConversionFahrenheit(Convert.ToDouble(Console.ReadLine())));
+            Console.WriteLine("Температура по шкале Фаренгейта: " + ConversionFahrenheit(Convert.ToDouble(Console.ReadLine())));
         }
 
         public static double ConversionKelvin(double x)
         {
-            double T = 273.15;
-            return (double) (x + T);
+            return (double)(x + 273.15);
         }
 
-        public static double ConversionFahrenheit (double x)
+        public static double ConversionFahrenheit(double x)
         {
             return (double)(1.8 * x + 32);
         }
