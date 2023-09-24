7f3eae5aac45e040b4bde2c5656387cc24029185
Mary250305 <slainter@mail.ru>
2023-09-24 14:35:56 +0700

    рассчет скидкb

---
+++ L6T5Discountt/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace L6T5Discount
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.Write("Введите сумму заказа:");
+            double price = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введите количество позиций:");
+            int quantity = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("К оплате " + ConversionDiscount(price, quantity));
+        }
+
+        public static double ConversionDiscount(double price, int quantity)
+        {
+            if (price >= 1000 && quantity >= 10)
+            {
+                return price - ((price * 10) / 100);
+            }
+            else if (price >= 1000 && quantity < 10)
+            {
+                return price - ((price * 5) / 100);
+            }
+            else if (price < 1000 && quantity >= 10)
+            {
+                return price - ((price * 5) / 100);
+            }
+            else
+            {
+                return price;
+            }
+        }
+    }
+}
