4f2e214d5435e51f0317eb703706fbba7ceae6c8
Mary250305 <slainter@mail.ru>
2023-09-24 13:47:37 +0700

    таблица

--- L5T8Table/Program.cs
+++ L5T8Table/Program.cs
@@ -6,18 +6,36 @@
         {
             Console.Write("До какого числа вывести таблицу: ");
             int number = Convert.ToInt32(Console.ReadLine());
-          
-            Console.WriteLine(string.Empty.PadLeft(number*10, '-'));
+
+            Console.Write("   | ");
 
             for (int i = 1; i <= number; i++)
             {
+                Console.Write(i.ToString().PadRight(4, ' '));
+            }
+
+            Console.WriteLine("");
+            Console.Write("---+");
+
+            for(int i = 0; i < number; i++)
+            {
+                Console.Write("----");
+            }
+
+            Console.WriteLine();
+
+            for (int i = 1; i <= number; i++)
+            {
+                Console.Write(" ");
+                Console.Write(i.ToString().PadRight(2, ' '));
+                Console.Write("| ");
+
                 for (int j = 1; j <= number; j++)
                 {
                     int result = i * j;
-                    Console.Write(result.ToString().PadRight(10));
+                    Console.Write(result.ToString().PadRight(4, ' '));
                 }
                 Console.WriteLine();
-                Console.WriteLine(string.Empty.PadLeft(number * 10, '-'));
             }
         }
     }
